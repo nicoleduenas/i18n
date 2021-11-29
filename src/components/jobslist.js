000f5c49db4905ea666db9a83f46ceb0358070eb
nv.duenas <nv.duenas@uniandes.edu.co>
2021-11-29 00:30:38 -0500

    Ejercicio

--- src/components/jobslist.js
+++ src/components/jobslist.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Job from "./job";
-
+import { FormattedMessage } from "react-intl";
+var userLanguage = window.navigator.userLanguage || window.navigator.language;
 const JobsList = () => {
   const [offers] = useState([
     {
@@ -10,6 +11,7 @@ const JobsList = () => {
       salary: 4.5,
       city: "Bogotá, Colombia",
       date: "2019-03-26",
+      views: 500,
     },
     {
       id: "0002",
@@ -18,6 +20,7 @@ const JobsList = () => {
       salary: 20,
       city: "Palo Alto, CA, USA",
       date: "2019-03-27",
+      views: 1250,
     },
     {
       id: "0003",
@@ -26,20 +29,34 @@ const JobsList = () => {
       salary: 1,
       city: "Cali, Colombia",
       date: "2019-03-28",
+      views: 120,
     },
   ]);
 
   return (
     <div>
       <table className="table">
-        <thead className="thead-dark">
+        <thead className={userLanguage === "en-US" ? "thead-dark" : "thead-light"}>
           <tr>
             <th scope="col">#</th>
-            <th scope="col">Position</th>
-            <th scope="col">Company</th>
-            <th scope="col">Salary</th>
-            <th scope="col">City</th>
-            <th scope="col">Publication date</th>
+            <th scope="col">
+              <FormattedMessage id="Position" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Company" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Salary" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="City" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="PublicationDate" />
+            </th>
+            <th scope="col">
+              <FormattedMessage id="Views" />
+            </th>
           </tr>
         </thead>
         <tbody>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
-
+import { IntlProvider } from "react-intl";
+import localeEsMessagesEs from "./locales/es";
+import localeEsMessagesEn from "./locales/en";
 import JobsList from "./components/jobslist";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(<JobsList />, document.getElementById("root"));
+var userLanguage = window.navigator.userLanguage || window.navigator.language;
+
+ReactDOM.render(
+  <IntlProvider
+    locale={userLanguage}
+    messages={
+      userLanguage === "en-US" ? localeEsMessagesEn : localeEsMessagesEs
+    }
+  >
+    <JobsList />
+  </IntlProvider>,
+  document.getElementById("root")
+);
